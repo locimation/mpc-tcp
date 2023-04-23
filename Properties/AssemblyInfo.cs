fa5417a213e81f4c7e7d58055498d9f9daf8320d
gdyr <m@gdyr.co>
2023-04-23 23:37:14 +1000

    Update AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 ﻿using System.Reflection;
 
 [assembly: AssemblyTitle("Crestron_TCP_Buttons")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Locimation Pty Ltd")]
 [assembly: AssemblyProduct("Crestron_TCP_Buttons")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © Locimation Pty Ltd 2023")]
 [assembly: AssemblyVersion("1.0.0.*")]
 
